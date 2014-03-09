fef44cdff3d7e2376de254c49d6350a3bd1c9f7c
julio <julio336@hotmail.com>
2014-03-08 23:05:15 -0600

    next calibration

--- db/migrate/20140308043726_change_date_format_in_devices.rb
+++ db/migrate/20140308043726_change_date_format_in_devices.rb
@@ -1,7 +1,10 @@
 class ChangeDateFormatInDevices < ActiveRecord::Migration
   def up
-  	change_column :devices, :next_calibration, :date
-  end
+	connection.execute(%q{
+    	alter table devices
+    	alter column next_calibration
+    	type date using cast(next_calibration as date)
+  	})  end
 
   def down
   	change_column :devices, :next_calibration, :string
