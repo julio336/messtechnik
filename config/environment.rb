55426017754b175dabdac7ae13f3de1f3cd22b85
julio <julio336@hotmail.com>
2014-05-28 22:07:47 -0500

    devise

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,4 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 Messtechnik::Application.initialize!
+config.action_mailer.default_url_options = { :host => 'calibraciones-messtechnik.com' }
