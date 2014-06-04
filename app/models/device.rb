8ef238f41f6bf8acd45401e5202536193d01547a
julio <julio336@hotmail.com>
2014-06-03 19:59:03 -0500

    changes

--- app/models/device.rb
+++ app/models/device.rb
@@ -1,7 +1,7 @@
 class Device < ActiveRecord::Base
   attr_accessible :brand, :calibration_date, :deflection, :description, :folio, :model, :next_calibration, :responsible, :user_id, :status
 
-  validates_presence_of :brand, :calibration_date, :deflection, :description, :folio, :model, :next_calibration, :responsible, :status
+  validates_presence_of :brand, :calibration_date, :deflections, :folio, :model, :next_calibration, :responsible, :status
 
   belongs_to :user
 
